2fc32e886ea3ed541eadc5eba6221a2fa87ab022
ramirochangomoreno <rama.exe.13@gmail.com>
2023-02-13 15:23:58 -0300

    escribiendo Faq y Contacto

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -17,30 +17,26 @@ export default function Menu() {
       name: "FAQ",
       img: "",
     },
+
     {
-      link: "./ofertas",
-      name: "Ofertas",
-      img: "",
-    },
-    {
-      link: "./contacto",
+      link: "./Contacto",
       name: "Contacto",
       img: "",
     },
   ];
 
   return (
-      <nav className="flex lg:w-4/12 flex-wrap items-center  justify-around text-lg capitalize  tracking-wider font-normal md:ml-auto">
+    <nav className="flex lg:w-4/12 flex-wrap items-center  justify-around text-lg capitalize  tracking-wider font-normal md:ml-auto">
       {" "}
-        {links.map((link) => (
-          <Link
+      {links.map((link) => (
+        <Link
           key={links.indexOf(link)}
-            href={link.link}
-            className=" hover:text-sky-700 hover:bg-transparent duration-300  "
-          >
-            {link.name}
-          </Link>
-        ))}
-      </nav>
+          href={link.link}
+          className=" hover:text-sky-700 hover:bg-transparent duration-300  "
+        >
+          {link.name}
+        </Link>
+      ))}
+    </nav>
   );
 }
--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,29 +1,204 @@
-import Image from "next/image";
-import Link from "next/link"
-import {fetchingData  ,fetchGoogle} from "../../services/fetchGoogleSheet";
-import Products from "../components/Products";
 export default async function Faq() {
-
-
-  const preguntando=await fetchGoogle()
-const datos=preguntando
   return (
-    <div>
-        <h1>FAQ</h1>
-        <div className="flex flex-wrap gap-1 mx-auto py-10 px-5 justify-around items-center">
-        {
-          !datos?
-          <span>Esperemoooos meeeen</span>
-          :
-          datos?.map((product,i)=>(
-            <Products 
-            item={product}
-            showAs={"card"}
-            key={i}
-            />
-          ))
-          }
-          </div>
-    </div>
-  )
+    <div className="w-6/12 mt-20">
+      <div className="">
+      <h2 className="font-semibold text-3xl  my-5">
+      ¿Quienes somos?
+      </h2>
+      <div className="bg-white border border-gray-200 divide-y divide-gray-200 rounded-xl">
+        <details className="group p-6 [&_summary::-webkit-details-marker]:hidden duration-300">
+          <summary className="flex items-center justify-between cursor-pointer">
+            <h2 className="text-lg font-medium text-gray-900">
+              Lorem ipsum dolor sit amet consectetur adipisicing?
+            </h2>
+
+            <span className="relative ml-1.5 h-5 w-5 flex-shrink-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <p className="mt-4 leading-relaxed text-gray-700">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
+            veritatis molestias culpa in, recusandae laboriosam neque aliquid
+            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
+            distinctio corporis earum similique!
+          </p>
+        </details>
+
+        <details className="group p-6 [&_summary::-webkit-details-marker]:hidden">
+          <summary className="flex items-center justify-between cursor-pointer">
+            <h2 className="text-lg font-medium text-gray-900">
+              Lorem ipsum dolor sit amet consectetur adipisicing?
+            </h2>
+
+            <span className="relative ml-1.5 h-5 w-5 flex-shrink-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <p className="mt-4 leading-relaxed text-gray-700">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
+            veritatis molestias culpa in, recusandae laboriosam neque aliquid
+            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
+            distinctio corporis earum similique!
+          </p>
+        </details>
+      </div></div>
+      <div className="">
+      <h2 className="font-semibold text-3xl  my-5">
+      ¿Como comprar por esta tienda?
+      </h2>
+      <div className="bg-white border border-gray-200 divide-y divide-gray-200 rounded-xl">
+        <details className="group p-6 [&_summary::-webkit-details-marker]:hidden duration-300">
+          <summary className="flex items-center justify-between cursor-pointer">
+            <h2 className="text-lg font-medium text-gray-900">
+              Lorem ipsum dolor sit amet consectetur adipisicing?
+            </h2>
+
+            <span className="relative ml-1.5 h-5 w-5 flex-shrink-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <p className="mt-4 leading-relaxed text-gray-700">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
+            veritatis molestias culpa in, recusandae laboriosam neque aliquid
+            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
+            distinctio corporis earum similique!
+          </p>
+        </details>
+
+        <details className="group p-6 [&_summary::-webkit-details-marker]:hidden">
+          <summary className="flex items-center justify-between cursor-pointer">
+            <h2 className="text-lg font-medium text-gray-900">
+              Lorem ipsum dolor sit amet consectetur adipisicing?
+            </h2>
+
+            <span className="relative ml-1.5 h-5 w-5 flex-shrink-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <p className="mt-4 leading-relaxed text-gray-700">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
+            veritatis molestias culpa in, recusandae laboriosam neque aliquid
+            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
+            distinctio corporis earum similique!
+          </p>
+        </details>
+      </div>
+    </div></div>
+  );
 }
